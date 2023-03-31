ba2fbc1029a2f04019567a968645e64a8ba43d7a
Mirella <mirella.dimichino@gmail.com>
2023-03-31 18:25:35 +0100

    added full navbar and removed some index.css lines in body

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,12 +10,12 @@ function ColorSchemesExample() {
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Opere</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <br />
+      
       
 
       
