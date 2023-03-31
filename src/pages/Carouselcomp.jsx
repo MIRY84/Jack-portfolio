f52bcbfc9cfc7a4506f0c7aec16ed95650dc235e
Mirella <mirella.dimichino@gmail.com>
2023-03-31 12:03:12 +0100

    added header/carousel and bootstrap, starter

---
+++ src/pages/Carouselcomp.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import item1 from "../images/favorites/Medusa.jpg";
+import item2 from "../images/favorites/Orca.jpg";
+import item3 from "../images/favorites/Octopus.jpg"
+
+function Carouselcomp() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-80" width={500} height={500}
+          src={item1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100" width={500} height={500}
+          src={item2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100" width={500} height={500}
+          src={item3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+
+export default Carouselcomp;
