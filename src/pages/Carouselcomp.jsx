dab773c42388c7fe1f6f8f2b6c653cce83cd8fe2
Mirella <mirella.dimichino@gmail.com>
2023-04-01 10:00:35 +0100

    changed names to photos and added more on carousel

--- src/pages/Carouselcomp.jsx
+++ src/pages/Carouselcomp.jsx
@@ -3,8 +3,14 @@ import Carousel from 'react-bootstrap/Carousel';
 import item1 from "../images/favorites/Medusa.jpg";
 import item2 from "../images/favorites/Orca.jpg";
 import item3 from "../images/favorites/Octopus.jpg";
-import item4 from "../images/favorites/Panda.jpg";
-import item5 from "../images/favorites/Spirali.jpg";
+import item4 from "../images/favorites/Octopustampsec.jpg";
+import item5 from "../images/favorites/Octopus-final.jpg";
+import item6 from "../images/favourites/itrearchi.jpg"
+import item7 from "../images/favourites/Spirali.jpg"
+import item8 from "../images/favourites/Panda.jpg"
+import item9 from "../images/favourites/Medusa.jpg"
+import item10 from "../images/favourites/Civetta.jpg"
+import item11 from "../images/favourites/itrearchi.jpg"
 import { Container } from 'react-bootstrap';
 
 function Carouselcomp() {
@@ -44,12 +50,24 @@ function Carouselcomp() {
       <Carousel.Item>
         <img
           className="" width={800} height={500}
+          src={item3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Octopus</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="" width={800} height={500}
           src={item4}
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Panda</h3>
+          <h3>Octopus finale</h3>
           
         </Carousel.Caption>
       </Carousel.Item>
@@ -57,6 +75,78 @@ function Carouselcomp() {
         <img
           className="" width={800} height={500}
           src={item5}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Octopus stampa a secco</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="" width={800} height={500}
+          src={item6}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>I tre archi</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="" width={800} height={500}
+          src={item7}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Spirali</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="" width={800} height={500}
+          src={item8}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Spirali</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="" width={800} height={500}
+          src={item9}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Spirali</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="" width={800} height={500}
+          src={item10}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Spirali</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="" width={800} height={500}
+          src={item11}
           alt="Third slide"
         />
 
