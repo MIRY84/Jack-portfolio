3ef4ab446663ff6d8f66125a050bd14426213a78
Mirella <mirella.dimichino@gmail.com>
2023-04-02 20:18:42 +0100

    removed better carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ import item8 from "../images/favorites/Panda.jpg";
 import item9 from "../images/favorites/mantaraya.jpg";
 import item10 from "../images/favorites/Civetta.jpg";
 import item11 from "../images/favorites/iltutto.jpg";
-import Carousel from 'better-react-carousel'
+
 
 const Home = () => {
   return (
@@ -24,27 +24,12 @@ const Home = () => {
       <img src={jack} width={400} height={300} />
 
 
-      <div>
-      <Carousel cols={2} rows={1} gap={10} loop>
-      <Carousel.Item>
-        <img width="100%" src="https://picsum.photos/800/600?random=1" />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img width="100%" src="https://picsum.photos/800/600?random=2" />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img width="100%" src="https://picsum.photos/800/600?random=3" />
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* anything you want to show in the grid */}
-      </Carousel.Item>
-      {/* ... */}
-    </Carousel>
+      
 
 
 
       </div>
-    </div>
+   
 
 
 
