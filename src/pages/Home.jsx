77423e37235bbeaed1a5c0e36f3a9ee89bf16bf6
Mirella <mirella.dimichino@gmail.com>
2023-04-03 12:37:19 +0100

    adding more effects like pagination on cover

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -62,6 +62,21 @@ const Home = () => {
             }}
             pagination={true}
             modules={[EffectCoverflow, Pagination]}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 5,
+                spaceBetween: 50,
+              },
+            }}
+          
             className="mySwiper"
           >
           
