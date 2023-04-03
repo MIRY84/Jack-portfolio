0f74ce1719400d9056a07b152e38f3b9a90dee1f
Mirella <mirella.dimichino@gmail.com>
2023-04-03 20:17:25 +0100

    changed suntax

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -100,36 +100,36 @@ const Home = () => {
           </Row>
           <Row>
             <Col sm={12}>
-            <p><span fontWeight style="bold">Giacomo Dimichino</span>
+            <p>Giacomo Dimichino
 Nato a Bari (BA) il 03/05/1991, consegue il diploma di maturità artistica “prima
 sezione Accademia” presso il Liceo Artistico Statale “C. Levi” di Matera (MT) nel
 2009, dove frequenta il corso di “Fumetto e Disegno Caricaturale” con Claudio
 Vino.
-Nel 2009 frequenta l’Accademia Di Belle Arti di Lecce (LE) indirizzo Decorazione
-con Paolo D’Orazio e Pippo Altomare, e nel 2013 frequenta un corso di Mosaico e
-partecipa ad un’esposizione “IDEM NUNC UNICUM” (identità non è singolarità) a
-cura di Lucy Ghionna presso l’associazione socio-artistico-culturale “Le ali di
+Nel 2009 frequenta Accademia Di Belle Arti di Lecce (LE) indirizzo Decorazione
+con Paolo D Orazio e Pippo Altomare, e nel 2013 frequenta un corso di Mosaico e
+partecipa ad un esposizione “IDEM NUNC UNICUM” (identità non è singolarità) a
+cura di Lucy Ghionna presso l associazione socio-artistico-culturale “Le ali di
 Pandora” a Matera.
-Dicembre 2016 Partecipa ad un’esposizione di pittura “Il Cinema” presso l’Abbazia
+Dicembre 2016 Partecipa ad un esposizione di pittura “Il Cinema” presso l Abbazia
 di S. Michele Arcangelo di Montescaglioso (Matera).
 Febbraio 2017 Partecipa ad un’esposizione di pittura “La Natura Morta” presso
 l’Abbazia di S. Michele Arcangelo di Montescaglioso (MT).
 Dal 2018 fa parte dell’Associazione Incisori Grafica Di Via Sette Dolori di Matera
 con i maestri Vittorio Manno e Angelo Rizzelli.
-Gennaio 2019 partecipa all’esposizione del “Calendario Degli Incisori Di Matera
+Gennaio 2019 partecipa all esposizione del “Calendario Degli Incisori Di Matera
 2019” presso la “Grafica di Via Sette Dolori” di Matera (MT).
-Marzo 2019 partecipa al Corso Intensivo “Libro D’Artista e Caratteri Mobili” diretto
+Marzo 2019 partecipa al Corso Intensivo “Libro D Artista e Caratteri Mobili” diretto
 dal maestro Alfredo Bartolomeoli presso la “Grafica di Via Sette Dolori” di Matera
 (MT).
 Ottobre 2019 è tra i selezionati per il XXXIII Premio Fibrenus “Carnello Carte ad
 Arte” XVI edizione (500anni) a Sora (FR).
-Dicembre 2019 partecipa all’esposizione del “Festival Internazionale del libro
-d’artista inciso” presso il Distretto del mobile imbottito a Matera.
+Dicembre 2019 partecipa all esposizione del “Festival Internazionale del libro
+d artista inciso” presso il Distretto del mobile imbottito a Matera.
 Dicembre 2021 in occasione della 17ma giornata del contemporaneo, espone tre
 incisioni a “La grafica si mostra” presso il “Distretto del mobile imbottito” a Matera
 (MT).
 Giugno 2022 è tra i selezionati al “XV Premio Acqui-Mostra Internazionale Biennale
-per l’incisione” ad Acqui Terme, Alessandria (AL).
+per l incisione” ad Acqui Terme, Alessandria (AL).
 
 Curriculum Artistico di Giacomo Dimichino
 giacomo.dimichino@gmail.com
