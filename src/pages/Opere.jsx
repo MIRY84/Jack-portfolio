b335fd0215d473eb09844b6318dabdddfcdefcf4
Mirella <mirella.dimichino@gmail.com>
2023-04-05 19:01:13 +0100

    added more pics in opere

--- src/pages/Opere.jsx
+++ src/pages/Opere.jsx
@@ -14,15 +14,35 @@ import { EffectCoverflow, Pagination } from "swiper";
 
 import item1 from "../images/favorites/Medusa.jpg";
 import item2 from "../images/favorites/Orca.jpg";
-import item3 from "../images/favorites/Octopus.jpg";
-import item4 from "../images/favorites/Octopustampsec.jpg";
-import item5 from "../images/favorites/Octopus-final.jpg";
-import item6 from "../images/favorites/itrearchi.jpg";
-import item7 from "../images/favorites/Spirali.jpg";
-import item8 from "../images/favorites/Panda.jpg";
-import item9 from "../images/favorites/mantaraya.jpg";
-import item10 from "../images/favorites/Civetta.jpg";
-import item11 from "../images/favorites/iltutto.jpg";
+import item3 from "../images/favorites/orca2.jpg";
+import item4 from "../images/favorites/Octopus.jpg";
+import item5 from "../images/favorites/Octopustampsec.jpg";
+import item6 from "../images/favorites/Octopus-final.jpg";
+import item7 from "../images/favorites/itrearchi.jpg";
+import item8 from "../images/favorites/Spirali.jpg";
+import item9 from "../images/favorites/Panda.jpg";
+import item10 from "../images/favorites/mantaraya.jpg";
+import item11 from "../images/favorites/Civetta.jpg";
+import item12 from "../images/favorites/conchiglia.jpg";
+import item13 from "../images/favorites/equilibriotiger.jpg";
+import item14 from "../images/favorites/cantosmeraldino.jpg";
+import item15 from "../images/favorites/Hippocampus.jpg";
+import item16 from "../images/favorites/Ex libris Jordan Petkov (corallo).jpg";
+import item17 from "../images/favorites/Maretempesta.jpg";
+import item18 from "../images/favorites/mareintempesta.jpg";
+import item19 from "../images/favorites/Ara Macao.jpg";
+import item20 from "../images/favorites/libroartista.jpg";
+import item21 from "../images/favorites/Attrazione sferica.jpg";
+import item22 from "../images/favorites/peacock1.jpg";
+import item23 from "../images/favorites/Peacock.jpg";
+import item24 from "../images/favorites/The eye of God.jpg";
+import item25 from "../images/favorites/argopanoptes.jpg";
+import item26 from "../images/favorites/axolotl.jpg";
+
+import item27 from "../images/favorites/iltutto.jpg";
+import item28 from "../images/favorites/iltutto2.jpg";
+import item29 from "../images/favorites/Capitale europea della cultura 2019.jpg";
+import item30 from "../images/favorites/Dai gradoni di Sant'Antonio.jpg";
 import { Container, Row, Col} from 'react-bootstrap';
 
 function Carouselcomp() {
@@ -86,6 +106,70 @@ function Carouselcomp() {
         <SwiperSlide>
           <img src={item9} />
         </SwiperSlide>
+        <SwiperSlide>
+          <img  src={item10} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={item11} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img  src={item12} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={item13} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img  src={item14} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={item15} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img  src={item16} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={item17} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img  src={item18} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={item19} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img  src={item20} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={item21} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img  src={item22} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={item23} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img  src={item24} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={item25} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img  src={item26} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={item27} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img  src={item28} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={item29} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={item30} />
+        </SwiperSlide>
+
       </Swiper>
       </Col>
       </Row>
