b46fdf353eda3aa12f3f18931155310995ae1c51
Mirella <mirella.dimichino@gmail.com>
2023-04-06 16:26:28 +0100

    added name bold and art print

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -92,8 +92,8 @@ const Home = () => {
         </Row>
         <Row>
           <Col sm={12}>
-            <p>Giacomo Dimichino<br />
-              Nato a Bari (BA) il 03/05/1991, consegue il diploma di maturità artistica “prima
+          <h2 className="text-center">Giacomo Dimichino Print Artist</h2><br />
+              <p>Nato a Bari (BA) il 03/05/1991, consegue il diploma di maturità artistica “prima
               sezione Accademia” presso il Liceo Artistico Statale “C. Levi” di Matera (MT) nel
               2009, dove frequenta il corso di “Fumetto e Disegno Caricaturale” con Claudio
               Vino. <br />
@@ -102,7 +102,7 @@ const Home = () => {
               partecipa ad un esposizione “IDEM NUNC UNICUM” (identità non è singolarità) a
               cura di Lucy Ghionna presso l associazione socio-artistico-culturale “Le ali di
               Pandora” a Matera.<br />
-              Dicembre 2016 Partecipa ad un esposizione di pittura “Il Cinema” presso l Abbazia
+              Nel Dicembre 2016 Partecipa ad un esposizione di pittura “Il Cinema” presso l Abbazia
               di S. Michele Arcangelo di Montescaglioso (Matera).<br />
               Febbraio 2017 Partecipa ad un’esposizione di pittura “La Natura Morta” presso
               l’Abbazia di S. Michele Arcangelo di Montescaglioso (MT).<br />
@@ -113,21 +113,18 @@ const Home = () => {
               Marzo 2019 partecipa al Corso Intensivo “Libro D Artista e Caratteri Mobili” diretto
               dal maestro Alfredo Bartolomeoli presso la “Grafica di Via Sette Dolori” di Matera
               (MT).<br />
-              Ottobre 2019 è tra i selezionati per il XXXIII Premio Fibrenus “Carnello Carte ad
+              Nell'Ottobre 2019 è tra i selezionati per il XXXIII Premio Fibrenus “Carnello Carte ad
               Arte” XVI edizione (500anni) a Sora (FR).<br />
-              Dicembre 2019 partecipa all esposizione del “Festival Internazionale del libro
+              Nel Dicembre 2019 partecipa all esposizione del “Festival Internazionale del libro
               d artista inciso” presso il Distretto del mobile imbottito a Matera.<br />
-              Dicembre 2021 in occasione della 17ma giornata del contemporaneo, espone tre
+              Nel Dicembre 2021 in occasione della 17ma giornata del contemporaneo, espone tre
               incisioni a “La grafica si mostra” presso il “Distretto del mobile imbottito” a Matera
               (MT).<br />
-              Giugno 2022 è tra i selezionati al “XV Premio Acqui-Mostra Internazionale Biennale
+              A Giugno 2022 è tra i selezionati al “XV Premio Acqui-Mostra Internazionale Biennale
               per l incisione” ad Acqui Terme, Alessandria (AL).<br />
-              <br />
-              Curriculum Artistico di Giacomo Dimichino<br />
-              giacomo.dimichino@gmail.com<br />
-              Ottobre 2022 in occasione della 18ma giornata mondiale del contemporaneo
+              Nell' Ottobre 2022 in occasione della 18ma giornata mondiale del contemporaneo
               espone 6 incisioni presso la “galleria Domus.Mad Art” e altre 2 incisioni presso il
-              “Distretto del mobile imbottito” a Matera</p>
+              “Distretto del mobile imbottito” a Matera.</p>
           </Col>
         </Row>
       </Container>
